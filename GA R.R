8bd5889801574c3674a709e8f29de584eafece7e
Aymeric Flaisler <aflaisler@gmail.com>
2015-06-30 14:38:13 +0100

    RGoogle-Analytics

---
+++ GA R.R
@@ -0,0 +1,16 @@
+#install dev tool to use GA in R
+install.packages("devtools")
+library(devtools)
+
+#install stringi
+install.packages("stringi")
+library(stringi)
+
+#install the github repo  to use GA API v3 (made by Googlers)
+install_github("Tatvic/RGoogleAnalytics")
+library(RGoogleAnalytics)
+
+#This is another repo with more stars
+#install_github("skardhamar/rga")
+#library(rga)
+
