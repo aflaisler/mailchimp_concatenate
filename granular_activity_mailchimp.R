b5b404a91a2b57a3b88aa0600e943404d953df01
Aymeric Flaisler <aflaisler@gmail.com>
2015-07-28 11:54:55 +0100

    added Test #1

--- granular_activity_mailchimp.R
+++ granular_activity_mailchimp.R
@@ -45,6 +45,7 @@ sanitizeUserfile <- function(filename) {
       ret[,c("email", "dateCreated", "id", "Type")]
 }
 
+
 #Rename colum and sanitize
 cleanUserfile <- function(filename){
       setwd("..")
@@ -55,22 +56,50 @@ cleanUserfile <- function(filename){
 
 
 directory <- "C:/Users/Aymeric/Documents/endource/cohort/25307411-6/granular_activity"
-setwd(directory)
-#get clicked email data
-dataClicks <- concatenateCSV_granular(getwd(), "clicks", "Clicks")
-#get opened email data
-dataOpens <- concatenateCSV_granular(getwd(), "opens", "Opens")
-#get new user data
-dataUsers <- cleanUserfile("users.csv")
 
-allData <- rbind(dataClicks, dataOpens, dataUsers)
-
-#export email data into csv
-extract_Name <- paste("c:/Temp/granular_mailchimp_data",
-                      format(Sys.Date(),"%d%m%y"),".csv",sep="_")
-write.csv(allData, extract_Name, row.names=FALSE)
 
 
 #function to run all the commands
+Granular_Activity_Data <- function(directory="C:/Users/Aymeric/Documents/endource/cohort/25307411-6/granular_activity"){
+      setwd(directory)
+      #get clicked email data
+      dataClicks <- concatenateCSV_granular(getwd(), "clicks", "Clicks")
+      #get opened email data
+      dataOpens <- concatenateCSV_granular(getwd(), "opens", "Opens")
+      #get new user data
+      dataUsers <- cleanUserfile("users.csv")
+      #concatenate all data
+      allData <- rbind(dataClicks, dataOpens, dataUsers)
+      
+      #export email data into csv
+      extract_Name <- paste("c:/Temp/granular_mailchimp_data",
+                            format(Sys.Date(),"%d%m%y"),".csv",sep="_")
+      write.csv(allData, extract_Name, row.names=FALSE)
+}
+
+
+#Test1 number of rows need to be sum of all files' rows
+Test1_Granular_Activity <- function(directory){ 
+      setwd(directory)
+      dataClicks <- concatenateCSV_granular(getwd(), "clicks", "Clicks")
+      dataOpens <- concatenateCSV_granular(getwd(), "opens", "Opens")
+      dataUsers <- cleanUserfile("users.csv")      
+      extract_Name <- paste("c:/Temp/granular_mailchimp_data",
+                            format(Sys.Date(),"%d%m%y"),".csv",sep="_")
+      test1_post <- nrow(read.csv(extract_Name))
+      
+      test1_pre <- (nrow(dataClicks)+nrow(dataOpens)+nrow(dataUsers))
+      
+      if(test1_post==test1_pre){
+            print("Passed")
+      }else{
+            print("Failed")
+      }
+}
+
+#test2  check 1st col only contain emails
+
+#test3 check 3rd col only contains "Clicks", "Opens", "New"
+
 
 
