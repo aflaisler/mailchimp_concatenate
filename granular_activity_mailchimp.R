92bc186f9dd915117a5ac3be719755e80337c60f
Aymeric Flaisler <aflaisler@gmail.com>
2015-07-28 15:46:08 +0100

    renamed all functions to respect lowerCamelCase and variable lowercases with underscores

--- granular_activity_mailchimp.R
+++ granular_activity_mailchimp.R
@@ -1,6 +1,6 @@
 
 #add column with filename
-read_csv_filename_granular <- function(filename, type){
+readFilenameGranular <- function(filename, type){
       ret <- read.csv(filename, header=F)[-1,c(1,3)]
       colnames(ret)[1:2] <- c("Timestamp", "email")
       ret$Source <- filename #EDIT
@@ -12,7 +12,7 @@ read_csv_filename_granular <- function(filename, type){
 
 
 #concatenate files with only email and click
-concatenateCSV_granular <- function(directory, folder="clicks", type="Clicks"){
+concatenateGranular <- function(directory, folder="clicks", type="Clicks"){
       dir <- paste(directory, folder, sep = "/")
       setwd(dir)
       p <- list.files(dir, pattern="*.csv")
@@ -22,7 +22,7 @@ concatenateCSV_granular <- function(directory, folder="clicks", type="Clicks"){
             temp1 <- read.csv(p[i], header=F)[,c(1,2)]
             if(nrow(temp1)!=1){
                   #import data and add column with file name
-                  temp2 <- read_csv_filename_granular(p[i], type)
+                  temp2 <- readFilenameGranular(p[i], type)
                   #concatenate
                   data <- rbind(data,temp2)
                   next
@@ -37,7 +37,7 @@ concatenateCSV_granular <- function(directory, folder="clicks", type="Clicks"){
 
 
 #read users.csv from mailchimp folder (added manually)
-sanitizeUserfile <- function(filename) {
+sanitizeUserFile <- function(filename) {
       ret <- read.csv("users.csv")[,c("id","email","dateCreated")]
       #ret$Data <- "1"
       ret$Type <- "New"
@@ -47,18 +47,18 @@ sanitizeUserfile <- function(filename) {
 
 
 #Rename colum and sanitize
-cleanUserfile <- function(filename){
+cleanUserFile <- function(filename){
       setwd("..")
-      temp <- sanitizeUserfile(filename)
+      temp <- sanitizeUserFile(filename)
       colnames(temp)[1:4] <- c("email","Timestamp", "Source", "Data")
       temp 
 }
 
 
 #Concatenate + extract
-Granular_Activity_Data <- function(directory){
+granularActivityData <- function(directory){
       #concatenate all data
-      allData <- rbind(dataClicks, dataOpens, dataUsers)
+      allData <- rbind(data_clicks, data_opens, data_users)
       #export email data into csv
       extract_Name <- paste("c:/Temp/granular_mailchimp_data",
                             format(Sys.Date(),"%d%m%y"),".csv",sep="_")
@@ -67,11 +67,11 @@ Granular_Activity_Data <- function(directory){
 
 
 #Test1 number of rows need to be sum of all files' rows
-Test1_Granular_Activity <- function(directory){      
+test1GranularActivity <- function(directory){      
       extract_Name <- paste("c:/Temp/granular_mailchimp_data",
                             format(Sys.Date(),"%d%m%y"),".csv",sep="_")
       test1_post <- nrow(read.csv(extract_Name))
-      test1_pre <- (nrow(dataClicks)+nrow(dataOpens)+nrow(dataUsers))
+      test1_pre <- (nrow(data_clicks)+nrow(data_opens)+nrow(data_users))
       if(test1_post==test1_pre){
             print("Passed")
       }else{
@@ -81,7 +81,7 @@ Test1_Granular_Activity <- function(directory){
 
 
 #test if 2 vectors of dim 3 are identical
-Test_Vector_dim3 <- function(X,Y){
+testVectorDim3 <- function(X,Y){
       error <- 0
       for(i in 1:3){
             if(Y[i,1]==X[i,1] & is.na(Y[i,1])==FALSE & is.na(X[i,1])==FALSE){
@@ -100,12 +100,12 @@ Test_Vector_dim3 <- function(X,Y){
 
 
 #test2  check 1st col only contain emails
-Test2 <- function(){
-      allData <- rbind(dataClicks, dataOpens, dataUsers)
+test2GranularActivity <- function(){
+      allData <- rbind(data_clicks, data_opens, data_users)
       X <- unique(allData[4])
       Data <- c("Clicks", "Opens", "New")
       Y <- data.frame(Data)
-      Test_Vector_dim3(X,Y)
+      testVectorDim3(X,Y)
 }
 
 
@@ -114,12 +114,12 @@ Test2 <- function(){
 #Extract and process data
 directory <- "C:/Users/Aymeric/Documents/endource/cohort/25307411-6/granular_activity"
 setwd(directory)
-dataClicks <- concatenateCSV_granular(getwd(), "clicks", "Clicks")
-dataOpens <- concatenateCSV_granular(getwd(), "opens", "Opens")
-dataUsers <- cleanUserfile("users.csv") 
-Granular_Activity_Data(directory)
-Test1_Granular_Activity(directory)
-
+data_clicks <- concatenateGranular(getwd(), "clicks", "Clicks")
+data_opens <- concatenateGranular(getwd(), "opens", "Opens")
+data_users <- cleanUserFile("users.csv") 
+granularActivityData(directory)
+test1GranularActivity(directory)
+test2GranularActivity()
 
 
 
@@ -129,5 +129,3 @@ test3 <- function(){
       col4 <- read.csv()
       if(typeof(directory)=="character"){print("passed")}
 }
-
-
